e83832bae5c7d0ab1fbd8b743c5f7dc48cfd69b6
Edward Chen <ejc042@ucsd.edu>
2020-01-03 21:40:28 -0800

    initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;        //Public variable to store a reference to the player game object
+
+    private Vector3 offset;          //Private variable to store the offset distance between the player and camera
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null) player = this.transform.parent.gameObject;
+        this.transform.position = player.transform.position + new Vector3(0,20,-3);
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        this.transform.rotation = Quaternion.Euler(80, 0, 0);
+    }
+}
---
+++ Assets/RW/Scripts/Movement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+
+    private bool attackMove = false;
+    private bool attacking = false;
+
+    private GameObject nextAttack;
+    private Vector3 nextMove;
+
+    private Camera cam;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (cam == null) cam = this.GetComponentInChildren<Camera>();
+        if (agent == null) agent = this.GetComponentInChildren<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        //All movement scripts
+        if (!attacking)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                attackMove = false;
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    agent.SetDestination(hit.point);
+                    nextMove = hit.point;
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (attackMove)
+                {
+                    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.transform != null
+                            && hit.transform.gameObject.name != "Ground")
+                        {
+                            if (Vector3.Distance(hit.transform.gameObject.transform.position, this.transform.position) < 2)
+                            {
+                                agent.SetDestination(this.transform.position);
+                                attackMove = false;
+                                StartCoroutine(Attack(hit.transform.gameObject));
+                            }
+                            else
+                            {
+                                agent.SetDestination(hit.point);
+                                nextAttack = hit.transform.gameObject;
+                                nextMove = hit.point;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (attackMove && nextAttack)
+            {
+                if (Vector3.Distance(nextAttack.transform.position, this.transform.position) < 2)
+                {
+                    agent.SetDestination(this.transform.position);
+                    attackMove = false;
+                    StartCoroutine(Attack(nextAttack));
+                    nextAttack = null;
+                }
+            }
+
+            if (this.transform.position == nextMove)
+            {
+
+            }
+        }
+
+        //key inputs
+        if (!attackMove && Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A key was pressed.");
+            attackMove = true;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            attackMove = false;
+        }
+    }
+
+    private IEnumerator Attack(GameObject obj)
+    {
+        attacking = true;
+        // process pre-yield
+        yield return new WaitForSeconds(0.5f);
+        obj.transform.position += new Vector3(0, -100, 0);
+        // process post-yield
+        attacking = false;
+    }
+}
